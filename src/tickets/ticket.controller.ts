6cfdefbb0c5c7f13c6386ea560c37be510bc1638
fuadfajris <fuadfajrisoba@gmail.com>
2025-10-06 01:49:30 +0700

    update api ticket

--- src/tickets/ticket.controller.ts
+++ src/tickets/ticket.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { TicketsService } from './ticket.service';
+import { Public } from '../common/decorators/public.decorators';
 
 @Controller('tickets')
 export class TicketsController {
   constructor(private readonly ticketsService: TicketsService) {}
 
+  @Public()
   @Get('event')
   async getTicketsByEvent(@Query('event_id') eventId: string) {
     return this.ticketsService.findByEvent(+eventId);
