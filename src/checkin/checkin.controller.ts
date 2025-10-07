9e4cfc4569717310a909c8119f78ee1c39c4c2e9
fuad.fajri <fuad.fajri@bni.co.id>
2025-10-07 10:09:41 +0700

    create checkin

--- src/checkin/checkin.controller.ts
+++ src/checkin/checkin.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { CheckinService } from './checkin.service';
+import { CreateCheckinDto } from './dto/create-checkin.dto';
 
 @Controller('checkins')
 export class CheckinController {
@@ -8,5 +9,15 @@ export class CheckinController {
   @Get()
   async getCheckins(@Query('event_id') eventId: string) {
     return this.checkinsService.getCheckinsByEvent(Number(eventId));
+  }
+
+  @Post()
+  async createCheckin(@Body() dto: CreateCheckinDto) {
+    return this.checkinsService.createCheckin(dto.ticket_detail_id);
+  }
+
+  @Get('all-checkins')
+  async getCheckinsV2(@Query('event_id') eventId: string) {
+    return this.checkinsService.getCheckinsByEventV2(Number(eventId));
   }
 }
